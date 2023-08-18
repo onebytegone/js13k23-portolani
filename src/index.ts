04a719badc22a188d3dd53e5ff90d6fd3cba6bef
Ethan Smith <ethan@onebytegone.com>
2023-08-18 09:20:44 -0400

    feat: switch to 8-dof movement

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { MovementSystem } from './systems/MovementSystem';
 import { RenderSystem } from './systems/RenderSystem';
 import { generateWorld } from './lib/generate-world';
 
-const FPS = 60,
-      worldState = generateWorld(0);
+const worldState = generateWorld(0);
 
 const systems = [
-   new InputSystem(),
    new MovementSystem(),
    new RenderSystem(),
+   new InputSystem(worldState, () => {
+      draw(Date.now());
+   }),
 ];
 
 let lastFrame = 0;
@@ -17,15 +18,9 @@ let lastFrame = 0;
 function draw(currentTime: number) {
    const delta = (currentTime - lastFrame) / 1000;
 
-   if (delta >= 1 / FPS) {
-      lastFrame = currentTime;
-
-      systems.forEach((system) => {
-         system.update(delta, worldState);
-      });
-   }
-
-   requestAnimationFrame(draw);
+   systems.forEach((system) => {
+      system.update(delta, worldState);
+   });
 }
 
-draw(0);
+draw(Date.now());
--- src/systems/InputSystem.ts
+++ src/systems/InputSystem.ts
@@ -2,61 +2,63 @@ import { ComponentID } from '@/shared-types';
 import { System } from './System';
 import { WorldState } from '@/lib/WorldState';
 
+function getHeadingFromKeyEvent(event: KeyboardEvent): { x: number; y: number } | undefined {
+   let x = 0, y = 0;
+
+   if (event.code === 'KeyA' || event.code === 'KeyQ' || event.code === 'KeyZ') {
+      x = -1;
+   }
+
+   if (event.code === 'KeyE' || event.code === 'KeyD' || event.code === 'KeyC') {
+      x = 1;
+   }
+
+   if (event.code === 'KeyQ' || event.code === 'KeyW' || event.code === 'KeyE') {
+      y = -1;
+   }
+
+   if (event.code === 'KeyZ' || event.code === 'KeyX' || event.code === 'KeyC') {
+      y = 1;
+   }
+
+   if (x || y) {
+      return { x, y };
+   }
+}
+
 export class InputSystem extends System {
 
    static components = [ ComponentID.Movement, ComponentID.Input ] as const;
 
-   private _state: Record<string, boolean> = {};
-
-   public constructor() {
+   public constructor(worldState: WorldState, onInput: () => void) {
       super();
 
       document.addEventListener('keydown', (event) => {
-         this._state[event.code] = true;
-      });
+         const heading = getHeadingFromKeyEvent(event);
 
-      document.addEventListener('keyup', (event) => {
-         this._state[event.code] = false;
-      });
-   }
+         if (heading) {
+            console.log(heading);
+            worldState.getEntities(InputSystem.components).forEach((entityID) => {
+               const [ movement ] = worldState.getComponents(entityID, InputSystem.components);
 
-   public update(_delta: number, worldState: WorldState): void {
-      worldState.getEntities(InputSystem.components).forEach((entityID) => {
-         const [ movement ] = worldState.getComponents(entityID, InputSystem.components);
+               movement.x = heading.x;
+               movement.y = heading.y;
+            });
 
-         if (this._isUpPressed()) {
-            movement.x = 0;
-            movement.y = -1;
-         } else if (this._isDownPressed()) {
-            movement.x = 0;
-            movement.y = 1;
-         } else if (this._isRightPressed()) {
-            movement.x = 1;
-            movement.y = 0;
-         } else if (this._isLeftPressed()) {
-            movement.x = -1;
-            movement.y = 0;
-         } else {
-            movement.x = 0;
-            movement.y = 0;
+            onInput();
          }
       });
    }
 
-   private _isUpPressed(): boolean {
-      return this._state['KeyW'] || this._state['KeyZ'] || this._state['ArrowUp'];
-   }
+   // eslint-disable-next-line class-methods-use-this
+   public update(_delta: number, worldState: WorldState): void {
+      worldState.getEntities(InputSystem.components).forEach((entityID) => {
+         const [ movement ] = worldState.getComponents(entityID, InputSystem.components);
 
-   private _isDownPressed(): boolean {
-      return this._state['KeyS'] || this._state['ArrowDown'];
-   }
-
-   private _isLeftPressed(): boolean {
-      return this._state['KeyA'] || this._state['KeyQ'] || this._state['ArrowLeft'];
-   }
-
-   private _isRightPressed(): boolean {
-      return this._state['KeyD'] || this._state['ArrowRight'];
+         // Stop all movement in prep for the next key up
+         movement.x = 0;
+         movement.y = 0;
+      });
    }
 
 }
